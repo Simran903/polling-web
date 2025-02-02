3698a0a14ad8bddb447916bc47ef98f49bdb089c
Simran <itssimran903@gmail.com>
2025-02-03 01:17:21 +0530

    client setup

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -11,6 +11,33 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+
+        primary: {
+          a0: "#d1ee26",
+          a10: "#d8f04b",
+          a20: "#def266",
+          a30: "#e4f47e",
+          a40: "#eaf695",
+          a50: "#eff7aa",
+        },
+
+        surface: {
+          a0: "#121212",
+          a10: "#282828",
+          a20: "#3f3f3f",
+          a30: "#575757",
+          a40: "#717171",
+          a50: "#8b8b8b",
+        },
+
+        tonal: {
+          a0: "#232418",
+          a10: "#38382d",
+          a20: "#4e4e44",
+          a30: "#64655c",
+          a40: "#7c7d75",
+          a50: "#95958f",
+        },
       },
     },
   },
