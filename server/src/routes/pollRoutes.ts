7a507b75641038ff9ee6988bfbde56e9dd13a276
Simran <itssimran903@gmail.com>
2025-02-18 02:26:06 +0530

    poll route fix

--- server/src/routes/pollRoutes.ts
+++ server/src/routes/pollRoutes.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import { protect } from '../middleware/authMiddleware';
-import { createPoll, getAllPolls, voteOnPoll } from '../controllers/pollController';
+import { createPoll, getAllPolls, getVotedPolls, voteOnPoll } from '../controllers/pollController';
 
 const router = express.Router();
 
 router.post("/create", protect as any, createPoll as any);
 router.get("/getAllPolls", protect as any, getAllPolls as any);
-// router.get("/votedPolls", protect as any, getVotedPolls as any);
+router.get("/votedPolls", protect as any, getVotedPolls as any);
 // router.get("/:id", protect as any, getPollById as any);
 router.post("/:id/vote", protect as any, voteOnPoll as any);
 // router.post("/:id/close", protect as any, closePoll as any);
